9ea163cf2fd520924d57f8850dd5d4c1926e7db8
Helen Abell <helen@chaosontoast.co.uk>
2025-01-17 22:21:31 +0000

    layout

--- gizmo101/src/App.js
+++ gizmo101/src/App.js
@@ -5,11 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
           Welcome to WellyGoat!
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
+
         <a
           className="App-link"
           href="https://reactjs.org"
