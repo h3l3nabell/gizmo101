0fcd30ae69a51eccc0ba189cca57a3345430a8f6
Helen Abell <helen@chaosontoast.co.uk>
2025-01-17 21:56:50 +0000

    Update images to temp goat

--- gizmo101/src/App.js
+++ gizmo101/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './WellyGoat2NoBG.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Welcome to WellyGoat!
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          Hello World here is Gizmo!
         </a>
       </header>
     </div>
